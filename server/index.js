83d604b85b769c2455f224ea35b82dcfbabeb117
oas <oas@st-andrews.ac.uk>
2019-03-02 16:04:20 +0000

    basic game with boxes

--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ const express = require('express');
 const app = express();
 
 let router = new express.Router();
+let api = new express.Router();
 
 app.use('/',router);
 app.use('/riphm',router);
+router.use('/api',api);
+
+// api.get('/level',(req,res)=>{
+//     res.json(new midiParse(req.query.file).getLevel());
+// });
 
 router.use('/',express.static('static'));
 
--- static/script.js
+++ static/script.js
@@ -1,8 +1,17 @@
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+
+    $('body').css('margin', '0px');
+
+    let level = new parser('a path').getLevel();
+
+    this.game = new Game(level);
 }
 
-function draw(){
-  background(100);
-  // box();
+
+let angle = Math.PI * 0.3;
+function draw() {
+    game.draw();
 }
